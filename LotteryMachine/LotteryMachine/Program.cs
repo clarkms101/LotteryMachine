88c377fc8ae5e0cc4bb75004e2f525b0970177bf
clarklin <clarkms101@gmail.com>
2022-10-31 15:17:30 +0800

    專案上傳

---
+++ LotteryMachine/LotteryMachine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LotteryMachine
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lotteryMachineLogic = new LotteryMachineLogic(100);
+
+            Console.WriteLine(lotteryMachineLogic.GetOneNumber());
+            Console.WriteLine(lotteryMachineLogic.GetOneNumber());
+
+            lotteryMachineLogic.Shuffle();
+
+            Console.WriteLine(lotteryMachineLogic.GetOneNumber());
+            Console.WriteLine(lotteryMachineLogic.GetOneNumber());
+            Console.WriteLine(lotteryMachineLogic.GetOneNumber());
+
+            lotteryMachineLogic.Shuffle();
+
+            var result = lotteryMachineLogic.GetAllNumber();
+            foreach (var s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
